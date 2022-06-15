87c53936e3dd81132690651a4c347604f8a3e956
Truong An from telio <truongan@telio.vn>
2022-06-16 02:37:27 +0700

    [main] deploy FE

--- src/share/config.ts
+++ src/share/config.ts
@@ -1,3 +1,2 @@
 export const NOTE_BACKEND_ENDPOINT: string =
-  process.env.API_URL ||
-  "https://fullstack-challenge-backend-99.herokuapp.com/";
+  process.env.API_URL || "https://fullstack-challenge-backend-99.herokuapp.com";
